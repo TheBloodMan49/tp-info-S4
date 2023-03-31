bab6dc310bfe84669d50eabad79612888b1d69bf
Paul Gasnier <paulgasnier49@gmail.com>
2023-03-31 16:42:16 +0200

    TP8 done

--- src/TP8/Polynome.java
+++ src/TP8/Polynome.java
@@ -1,103 +1,136 @@
 package TP8;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 
 public class Polynome {
-    private List<Double> coeffs;
+    private SortedMap<Integer, Double> coeffs;
 
     public Polynome() {
-        coeffs = new ArrayList<Double>();
-    }
-
-    public Polynome(List coeffs) {
-        this.coeffs = new ArrayList<Double>(coeffs);
+        this.coeffs = new TreeMap<Integer, Double>(Collections.reverseOrder());
     }
 
     public Polynome(Polynome p) {
-        this.coeffs = new ArrayList<Double>(p.coeffs);
-    }
-
-    public Polynome(double[] coeffs) {
         this();
-        for (int i = 0; i < coeffs.length; i++) {
-            this.coeffs.add(coeffs[i]);
-        }
+        this.coeffs.putAll(p.coeffs);
     }
 
     public double get(int i) {
-        return this.coeffs.get(i);
+        if (!this.coeffs.containsKey(i))
+            return 0.0;
+        else
+            return this.coeffs.get(i);
     }
 
     public void set(int i, double value) {
-        this.coeffs.set(i, value);
+        this.coeffs.put(i, value);
     }
 
     public int degree() {
-        return this.coeffs.size() - 1;
+        return this.coeffs.lastKey();
     }
 
     public Polynome add(Polynome p) {
-        Polynome result = new Polynome();
-        int i = 0;
-        while (i <= this.degree() && i <= p.degree()) {
-            result.coeffs.add(this.get(i) + p.get(i));
-            i++;
+        Polynome result = new Polynome(this);
+        for (Entry<Integer, Double> entry
+                : p.coeffs.entrySet()) {
+            result.inc(entry.getKey(), entry.getValue());
         }
-        while (i <= this.degree()) {
-            result.coeffs.add(this.get(i));
-            i++;
-        }
-        while (i <= p.degree()) {
-            result.coeffs.add(p.get(i));
-            i++;
+        return result;
+    }
+
+    public Polynome sub(Polynome p) {
+        Polynome result = new Polynome(this);
+        for (Entry<Integer, Double> entry : p.coeffs.entrySet()) {
+            result.inc(entry.getKey(), -entry.getValue());
         }
         return result;
     }
 
     public void inc(int i, double value) {
-        this.coeffs.set(i, this.coeffs.get(i) + value);
+        this.coeffs.put(i, this.get(i) + value);
     }
 
     public Polynome mul(Polynome p) {
         Polynome result = new Polynome();
-        for (int i = 0; i <= this.degree() + p.degree(); i++) {
-            result.coeffs.add(0.0);
-        }
-        for (int i = 0; i <= this.degree(); i++) {
-            for (int j = 0; j <= p.degree(); j++) {
-                result.inc(i + j, this.get(i) * p.get(j));
+        for (Entry<Integer, Double> entry1 : this.coeffs.entrySet()) {
+            for (Entry<Integer, Double> entry2 : p.coeffs.entrySet()) {
+                result.inc(entry1.getKey() + entry2.getKey(), entry1.getValue() * entry2.getValue());
             }
         }
         return result;
     }
 
-    public Polynome extract(int i, int j) {
+    public Polynome extract(int start, int length) {
         Polynome result = new Polynome();
-        for (int k = i; k <= j; k++) {
-            result.coeffs.add(this.get(k));
+        for (int k = start; k < start+length; k++) {
+            if (this.get(k) != 0) result.coeffs.put(k-start, this.get(k));
+        }
+        return result;
+    }
+
+    public Polynome leftShift(int n) {
+        Polynome result = new Polynome();
+        for (Entry<Integer, Double> entry : this.coeffs.entrySet()) {
+            result.coeffs.put(entry.getKey() + n, entry.getValue());
         }
         return result;
     }
 
     public Polynome mulK(Polynome p) {
-        // TODO
-        return new Polynome();
+        int n = Math.max(this.degree(), p.degree()) + 1;
+        return this.mulKRec(p, n);
+    }
+
+    // Recursive method to multiply two polynomials using Divide and Conquer
+    public Polynome mulKRec(Polynome p, int n) {
+        if (n == 1) {
+            return this.mul(p);
+        }
+        Polynome P0 = this.extract(0, n/2);
+        Polynome P1 = this.extract(n/2, n - n/2);
+
+        Polynome Q0 = p.extract(0, n/2);
+        Polynome Q1 = p.extract(n/2, n - n/2);
+
+        Polynome P0Q0 = P0.mulKRec(Q0, n/2);
+        Polynome P1Q1 = P1.mulKRec(Q1, n - n/2);
+        Polynome P0sumP1 = P0.add(P1);
+        Polynome Q0sumQ1 = Q0.add(Q1);
+
+        Polynome middlebit = P0sumP1.mulKRec(Q0sumQ1, n/2).sub(P0Q0).sub(P1Q1);
+
+        return P0Q0.add(middlebit.leftShift(n/2)).add(P1Q1.leftShift(n));
+
     }
 
     public String toString() {
         String result = "";
-        for (int i = this.degree(); i >= 0; i--) {
-            result += this.get(i) + (i > 0 ? "x^" + i + " + " : "");
+        for (Entry<Integer, Double> entry : this.coeffs.entrySet()) {
+            result += entry.getValue() + "x^" + entry.getKey() + " + ";
         }
         return result;
     }
 
     public static void main(String[] args) {
-        Polynome p1 = new Polynome(new double[] {1, 2, 3});
-        Polynome p2 = new Polynome(new double[] {1, 2, 3, 4});
+        Polynome p1 = new Polynome();
+        Polynome p2 = new Polynome();
+
+        p1.set(0,2);
+        p1.set(2,1);
+
+        p2.set(0,2);
+        p2.set(1,3);
+        p2.set(2,2);
+
+        System.out.println(p1.degree());
         System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p1.add(p2));
-        System.out.println(p1.mul(p2));
+        System.out.println(p1.get(1));
+        Polynome p3 = p1.add(p2);
+        System.out.println(p3);
+        System.out.println(p1.mulK(p3));
+        System.out.println(p1.mul(p3));
     }
 }
